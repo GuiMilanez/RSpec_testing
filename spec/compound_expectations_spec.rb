fea4acc28e598be35757d1b667a874632868ecca
Gui <glmilanez@gmail.com>
2019-05-19 19:35:19 -0400

    finish section 3

---
+++ spec/compound_expectations_spec.rb
@@ -0,0 +1,32 @@
+RSpec.describe 25 do
+
+  it 'can test for multiple matchers' do
+    # expect(subject).to be_odd
+    # expect(subject).to be > 20
+
+    expect(subject).to be_odd.and be > 20
+  end
+
+  it {is_expected.to be_odd.and be > 20}
+end 
+
+RSpec.describe 'caterpillar' do
+  it 'supports multiple matchers on a single line' do
+    expect(subject).to start_with('cat').and end_with('pillar')
+  end
+
+  it { is_expected.to start_with('cat').and end_with('pillar')}
+
+end
+
+RSpec.describe 20 do
+
+  it { is_expected.to be_even.and respond_to(:times) }
+  
+end
+
+RSpec.describe [4, 8, 15, 16, 23, 42] do
+
+  it { is_expected.to include(42).and start_with(4,8,15) }
+  
+end
---
+++ spec/not_to_spec.rb
@@ -0,0 +1,6 @@
+RSpec.describe 'not_to method' do
+
+  it 'checks for the inverse of a matcher' do
+    expect(5).not_to eq(10)
+  end
+end
